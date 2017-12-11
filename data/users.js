a1e84646d38878f0cee6ca54a60702128c3cd581
Pierre IFOCOP <p.vautherin@hotmail.com>
2017-12-11 10:20:11 +0100

    adding timer module

---
+++ data/users.js
@@ -0,0 +1,19 @@
+
+
+// note : JSX escapes any malicious code
+const users = {
+  id_1: {
+    name: 'Pierre',
+    age: 33,
+    img: "spider.jpg"
+  },
+
+  id_2: {
+    name: 'Brendan Eich',
+    age: 57,
+    img: "js.jpg"
+  }
+}
+
+
+const currentStatus = {no: "Pas d'incident en cours"}
---
+++ script/timer.js
@@ -0,0 +1,25 @@
+
+
+
+class Clock extends React.Component {
+
+  // call super on props and assign state
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+    }
+
+  // when component is mounted, it launches a delayed setState
+  // setState will call render
+  componentDidMount() {
+    this.timerID =  setInterval(
+      () =>  this.setState({ date: new Date() })
+        , 1000)}
+
+  // when unmounted, setInterval is cleared
+  componentWillUnmount() {clearInterval(timerID)}
+
+  render() {
+    return (<span>  { this.state.date.toLocaleString() }  </span>);
+    }
+}
