f12dd909cd1a3837e1ad8cf803171e5933464b5b
Carburator 11 <p.vautherin@hotmail.com>
2017-12-12 15:47:54 +0100

    layout change

--- data/users.js
+++ data/users.js
@@ -3,32 +3,82 @@
 // note : JSX escapes any malicious code
 const users = [
    {id: 00,
-    name: "Carol",
-    img: "img/spider.jpg",
+    name: "Eich Brendan",
+    img: "img/js.jpg",
     loc : "Paris 75019",
     tel : ""
   },
 
   { id : 1,
-    name: "Brendan Eich",
-    img: "img/js.jpg",
-    loc : "Los Angeles area",
+    name: "Berners-Lee Tim",
+    img: "img/TimBL.jpg",
+    loc : "London area",
     tel : ""
   },
 
   { id : 2,
-    name: "Don Draper",
-    img: "img/dd.jpg",
-    loc : "Manhattan, New York City",
+    name: "Torvald Linus ",
+    img: "img/LT.jpg",
+    loc : "Manhattan, NYC",
     tel : ""
   },
 
   { id : 3,
-    name: "David D.",
-    img: "img/dd2.jpg",
+    name: "Resig John",
+    img: "",
     loc : "London",
     tel : ""
   },
+
+  {id: 4,
+   name: "Lerdorf Rasmus ",
+   img: "",
+   loc : "Paris 75019",
+   tel : ""
+ },
+
+ { id : 5,
+   name: "Brendan Eich",
+   img: "",
+   loc : "Los Angeles area",
+   tel : ""
+ },
+
+ { id : 6,
+   name: "Don D.",
+   img: "img/dd.jpg",
+   loc : "NYC",
+   tel : ""
+ },
+
+ { id : 7,
+   name: "Fox M.",
+   img: "img/dd2.jpg",
+   loc : "DC Area",
+   tel : ""
+ },
+
+ { id : 8,
+   name: "Jimmy M.",
+   img: "img/MNLT.jpg",
+   loc : "West Baltimore",
+   tel : ""
+ },
+
+ { id : 9,
+   name: "Gérard De Halle",
+   img: "",
+   loc : "",
+   tel : ""
+ },
+
+ { id : 10,
+   name: "Chris Doe",
+   img: "img/spiderdd2.jpg",
+   loc : "London",
+   tel : ""
+ },
+
 
 ]
 
