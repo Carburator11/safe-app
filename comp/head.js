881b96b4aaf8b9feddcbf0dfbdfcecd6457d546d
Pierre IFOCOP <p.vautherin@hotmail.com>
2017-12-13 11:26:53 +0100

    modularization...

---
+++ comp/head.js
@@ -0,0 +1,11 @@
+// Title + Date
+function Head(){
+  return(<div className = "head">
+    <div className = "logo"><img src="img/logo.png" /></div>
+    <div className="title">
+        <div id="copyright"> &#9400;Carb11. 2017 – v0.1</div>
+        <h1>Safe app</h1>
+        <Clock />  {/* Extern*/}
+    </div>
+    </div>)
+  }
